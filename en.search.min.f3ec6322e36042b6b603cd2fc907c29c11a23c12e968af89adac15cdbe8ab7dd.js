f6f1141d1896c9d1d04bd7a01229d4a8c84b9f16
Per Lundberg <perlun@gmail.com>
2020-03-03 23:34:46 +0200

    Rebuilt from 6318694

--- en.search.min.f3ec6322e36042b6b603cd2fc907c29c11a23c12e968af89adac15cdbe8ab7dd.js
+++ en.search.min.f3ec6322e36042b6b603cd2fc907c29c11a23c12e968af89adac15cdbe8ab7dd.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.4c9045937a3623b6c9e5ba521ab8f1da24e5d64cb50f4b4d70c2654a22c6eb40.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.16a1642c892d5a71b5fca859a6cad8bc46daf4375860bc71749a51d0ad524890.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
